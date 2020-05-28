95226227943f13852f9fe1718a5839f9a0cb0c7b
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-28 17:55:01 +0530

    Make basic Scaffold

---
+++ lib/App.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class app extends StatefulWidget {
+  @override
+  _appState createState() => _appState();
+}
+
+class _appState extends State<app> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(color: Colors.white,),
+    );
+  }
+}
