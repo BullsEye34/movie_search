0fda4594abb655d3c4b65c1ec93181dd9e6d9763
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-28 18:07:30 +0530

    Pass in Search Text

--- lib/App.dart
+++ lib/App.dart
@@ -13,8 +13,8 @@ class app extends StatefulWidget {
 
 class _appState extends State<app> {
   var movies = new List<Movie>();
-  _getMovies() {
-    API.getUsers().then((response) {
+  _getMovies(var search) {
+    API.getUsers(search).then((response) {
       var o = response;
       //print(o.body.toString().substring(3));
       o = o.body.toString().substring(3);
--- lib/Models/API.dart
+++ lib/Models/API.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
+
 class API {
-  static Future getUsers() {
+  static Future getUsers(search) {
     var url = "https://melange2020.in/Reg/data.json";
     print(http.get(url));
     return http.get(url);
