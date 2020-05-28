53764d28d8db66b0c882b70a4d9b6255438aebd8
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-28 18:08:47 +0530

    Change API

--- lib/Models/API.dart
+++ lib/Models/API.dart
@@ -2,7 +2,10 @@ import 'package:http/http.dart' as http;
 
 class API {
   static Future getUsers(search) {
-    var url = "https://melange2020.in/Reg/data.json";
+    var url =
+        "https://api.themoviedb.org/3/search/movie?api_key=43236c9b4ffaa78012ee092b4e4f74d8&language=en-US&query=" +
+            search.toString() +
+            "&page=1&include_adult=true";
     print(http.get(url));
     return http.get(url);
   }
