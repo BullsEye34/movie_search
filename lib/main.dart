d6f29bcc7475b10400c41813f3d44d1a6a3820e1
Vamshi Prasad <vamvijkum@gmail.com>
2020-05-28 18:06:15 +0530

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'App.dart';
 
-void main(){
-  runApp(MaterialApp(home: app(),debugShowCheckedModeBanner: false,),);
+void main() {
+  runApp(
+    MaterialApp(
+      home: app(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
